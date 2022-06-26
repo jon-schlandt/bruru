e094c89eace6e26ced15574bdc01e8a3afa65487
schlanjo <schlanjo@gmail.com>
2022-06-26 18:23:52 -0500

    Add boilerplate project files

---
+++ Bruru/Bruru/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bruru
+//
+//  Created by Jon Schlandt on 6/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
